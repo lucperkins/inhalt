f307b484dc2c2e7f86e84a71ec45f9ea13853dda
Luc Perkins <lucperkins@gmail.com>
2022-02-18 16:59:38 -0800

    Get test coverage up to 100%

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,14 @@
-import { assert, test } from "vitest";
+import { assert, expect, test } from "vitest";
 
 import { Document, Documents, documents, File, Files, files } from "../src";
+import * as Lib from "../src";
+
+test("exports", () => {
+  expect(typeof Lib).to.equal("object");
+  Object.keys(Lib).forEach((key) => {
+    expect(Boolean(Lib[key])).toBe(true);
+  });
+});
 
 test("globs", async () => {
   const expectedFiles: File[] = [
@@ -22,6 +30,34 @@ test("globs", async () => {
 
   const mdFiles: Files = await files("test/_files/**/*.{md,mdx}");
   assert.sameDeepMembers(mdFiles.all(), expectedFiles);
+
+  const expectedPaths: string[] = [
+    "test/_files/index.md",
+    "test/_files/nested/getting-started.mdx",
+  ];
+  assert.sameDeepMembers(mdFiles.paths(), expectedPaths);
+
+  const expectedDocs: Document[] = [
+    {
+      name: "index",
+      dir: "test/_files",
+      path: "test/_files/index.md",
+      extension: ".md",
+      raw: "# The documentation\n\nWelcome to the documentation!\n",
+      slug: "test/_files/index",
+      html: "<h1>The documentation</h1>\n<p>Welcome to the documentation!</p>",
+    },
+    {
+      name: "getting-started",
+      dir: "test/_files/nested",
+      path: "test/_files/nested/getting-started.mdx",
+      extension: ".mdx",
+      raw: "---\ntitle: Getting started\n---\n\nThis is how you get started.\n",
+      slug: "test/_files/nested/getting-started",
+      html: "<hr>\n<h2>title: Getting started</h2>\n<p>This is how you get started.</p>",
+    },
+  ];
+  assert.sameDeepMembers(await mdFiles.documents(), expectedDocs);
 });
 
 test("markdown", async () => {
